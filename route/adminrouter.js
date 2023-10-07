fa4893008f9e3e0856b2d8b49d3378b92402008f
soura <souradipbabay2000@gmail.com>
2023-10-07 15:38:32 +0530

    update views

--- route/adminrouter.js
+++ route/adminrouter.js
@@ -10,6 +10,12 @@ router.get('/admin/apilog', login.apilog);
 router.get('/admin/apiopadd', login.apiopeator);
 router.get('/admin/useradd', login.useradd);
 router.get('/admin/userlist', login.userlist);
+router.get('/admin/operatorlist', login.operatorlist);
+router.get('/admin/amountwiseop', login.amountwise);
+router.get('/admin/apiswitch', login.apiswitch);
+router.get('/admin/mobile/history', login.rechchargehis);
+router.get('/admin/dth/history', login.dthhis);
+router.get('/admin/wallet/history', login.wallethis);
 // router.get('/admin/dashboard', login.dashboard);
 // router.get('/admin/dashboard', login.dashboard);
 
