9d0eead40c138b7fbf894dcd6ab0379a002664b9
soura <souradipbabay2000@gmail.com>
2023-10-07 15:32:11 +0530

    add frain

--- contollers/admin/logincontroller.js
+++ contollers/admin/logincontroller.js
@@ -23,6 +23,25 @@ class Logincontroller{
     async userlist(req, res) {
         res.render('admin/user/userlist')
     }
+    async operatorlist(req, res) {
+        res.render('admin/operator/operatorlist')
+    }
+    async amountwise(req, res) {
+        res.render('admin/operator/amountwiseapi')
+    }
+    async apiswitch(req, res) {
+        res.render('admin/operator/apiswitch')
+    }
+    async rechchargehis(req, res) {
+        res.render('admin/history/mobilehis')
+    }
+    async dthhis(req, res) {
+        res.render('admin/history/dthhis')
+    }
+    async wallethis(req, res) {
+        res.render('admin/history/wallethis')
+    }
+
 
 }
 module.exports = new Logincontroller();
