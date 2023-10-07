4172c902fdc649d360666905622e90ca0505cfa3
soura <souradipbabay2000@gmail.com>
2023-10-07 12:32:47 +0530

    update database

---
+++ config/datab.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connection_string = 'mongodb+srv://' + process.env.DBUSERNAME + ':' + process.env.PASSWORD + '@rechageapiresell.3kxesjj.mongodb.net/' + process.env.DB_NAME + '?retryWrites=true&w=majority';
+
+let option = {
+    auth: {
+        username: process.env.DBUSERNAME,
+        password: process.env.PASSWORD
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+module.exports = () => {
+    try {
+        mongoose.connect(connection_string, option);
+        console.log("DB connected successfully!");
+    } catch (err) {
+        console.log(err);
+    }
+}
